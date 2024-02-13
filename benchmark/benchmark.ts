e1f74aa185fd87a4352e4301b5c9e00e960debcd
Peter Robertson <peterjdrb@outlook.com>
2024-02-13 14:20:31 +0000

    check performance differences

--- benchmark/benchmark.ts
+++ benchmark/benchmark.ts
@@ -1,17 +1,35 @@
-import fs from "fs";
+// ------------------------------- NODE MODULES -------------------------------
+
+import { promises as fs } from "fs";
 import { performance, PerformanceObserver, PerformanceEntry } from "perf_hooks";
 
+// ------------------------------ CUSTOM MODULES ------------------------------
+
+// -------------------------------- VARIABLES ---------------------------------
+
+interface TestResult {
+  name: string;
+  averageInMS: number;
+  timeImproved: string;
+}
+
+interface PerformanceDiff extends TestResult {
+  percentageChange?: string;
+}
+
 const numberOfRunsToExecute = 3;
+const noticeableThreshold = 0.5;
+
+// ----------------------------- FILE DEFINITION ------------------------------
 
 const importFiles = async () => {
-  const files = fs
-    .readdirSync("./src/", { recursive: true })
-    .filter((file) => (file as string).endsWith(".perf.test.ts"));
+  let files = await fs.readdir("./src/", { recursive: true });
+  files = files.filter((file) => (file as string).endsWith(".perf.test.ts"));
 
   const tests: Array<[string | Buffer, any]> = [];
   for (const file of files) {
     const importedTests = await import(`../src/${file}`);
-    tests.push([file, importedTests.performanceTest]);
+    tests.push([`src\\${file}`, importedTests.performanceTest]);
   }
 
   return tests;
@@ -22,7 +40,14 @@ const runPerformanceTests = (performanceTests: [string | Buffer, any][]) => {
   const observer = new PerformanceObserver(() => {});
   observer.observe({ entryTypes: ["measure"], buffered: true });
 
-  performanceTests.forEach(([file, performanceTest]) => {
+  for (const fileIndex in performanceTests) {
+    const [file, performanceTest] = performanceTests[fileIndex];
+    console.log(
+      `Running performance test ${Number(fileIndex) + 1} of ${
+        performanceTests.length
+      }`
+    );
+
     for (let i = 0; i < numberOfRunsToExecute; i++) {
       if (!performanceTest) {
         console.error(
@@ -36,32 +61,93 @@ const runPerformanceTests = (performanceTests: [string | Buffer, any][]) => {
       performance.mark("end");
       performance.measure(file as string, "start", "end");
     }
-  });
+  }
 
   return performance.getEntriesByType("measure");
 };
 
-const colaltedTestResults = (results: PerformanceEntry[]) => {
+const collatedTestResults = (results: PerformanceEntry[]): TestResult[] => {
   const uniqueFileNames = [...new Set(results.map((element) => element.name))];
 
   return uniqueFileNames.flatMap((name) => {
     const allRuns = results.filter((result) => result.name === name);
 
-    const average =
+    const averageInMS =
       allRuns.reduce((acc, result) => {
         return acc + result.duration;
       }, 0) / numberOfRunsToExecute;
 
-    return { name, average };
+    return { name, averageInMS, timeImproved: new Date().toISOString() };
   });
+};
+
+const processResults = async (results: TestResult[]) => {
+  const isNoticeableDifference = (previous: number, current: number) => {
+    return Math.abs(current - previous) / previous > noticeableThreshold;
+  };
+
+  const previousResults: TestResult[] = JSON.parse(
+    await fs.readFile("./benchmark/results.json", "utf-8")
+  );
+
+  const newPerformanceResults: PerformanceDiff[] = [];
+  const improvedPerformance: PerformanceDiff[] = [];
+  const decreasedPerformance: PerformanceDiff[] = [];
+
+  results.forEach((result) => {
+    const wasPreviousResult = previousResults.find(
+      (previousResult) => previousResult.name === result.name
+    );
+
+    if (!wasPreviousResult) {
+      //New performance test or was not previously recorded
+      newPerformanceResults.push(result);
+    } else {
+      const timeDifference = result.averageInMS - wasPreviousResult.averageInMS;
+      const isFaster = result.averageInMS < wasPreviousResult.averageInMS;
+      const timeDifferencePercentage = (
+        (Math.abs(timeDifference) / wasPreviousResult.averageInMS) *
+        100
+      ).toFixed(2);
+
+      if (
+        isFaster &&
+        isNoticeableDifference(
+          wasPreviousResult.averageInMS,
+          result.averageInMS
+        )
+      ) {
+        //Improved performance
+        improvedPerformance.push({
+          ...result,
+          percentageChange: `${timeDifferencePercentage}%`,
+        });
+      } else if (
+        !isFaster &&
+        isNoticeableDifference(
+          wasPreviousResult.averageInMS,
+          result.averageInMS
+        )
+      ) {
+        //Decreased performance
+        decreasedPerformance.push({
+          ...result,
+          percentageChange: `${timeDifferencePercentage}%`,
+        });
+      }
+    }
+  });
+
+  console.log("newPerformanceResults: ", newPerformanceResults);
+  console.log("improvedPerformance: ", improvedPerformance);
+  console.log("decreasedPerformance: ", decreasedPerformance);
 };
 
 const main = async () => {
   const performanceTests = await importFiles();
   const results = runPerformanceTests(performanceTests);
-  const tabledResults = colaltedTestResults(results);
-
-  console.table(tabledResults);
+  const tabledResults = collatedTestResults(results);
+  await processResults(tabledResults);
 };
 
 main();
