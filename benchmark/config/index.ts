1802166ac06e3a9b9c68217f2f137323ea0ee5ed
Peter Robertson <peterjdrb@users.noreply.github.com>
2024-02-19 15:49:34 +0000

    added a noticeable threshold cli arg

--- benchmark/config/index.ts
+++ benchmark/config/index.ts
@@ -1,10 +1,23 @@
 export const numberOfRunsToExecute = 3;
-export const noticeableThreshold = 0.1;
 
 export const commandArgs = () => {
     const args = process.argv.slice(2);
 
     return {
         dryRun: args.findIndex((arg) => arg.toLowerCase() === '--dry-run') !== -1,
+        threashhold: () => {
+            const cliParam = '-noticeableThreshold=';
+            const passedIn = args.find((arg) => arg.toLowerCase().startsWith(cliParam.toLowerCase()));
+            let noticeableThreshold = 0.1;
+
+            if (passedIn) {
+                noticeableThreshold = +passedIn.split("=")[1];
+                if (isNaN(noticeableThreshold)) {
+                    console.error("The noticeableThreshold arg must be a decimal between 0 and 1 inclusive")
+                }
+            }
+
+            return noticeableThreshold
+        },
     }
 }
