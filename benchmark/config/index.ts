079eba63759e73c0f4af682b84225c3fd6eb3322
Peter Robertson <peterjdrb@users.noreply.github.com>
2024-02-20 16:18:57 +0000

    updated error message

--- benchmark/config/index.ts
+++ benchmark/config/index.ts
@@ -4,7 +4,7 @@ const defaultThreshold = 0.1
 
 const threashhold = (threshold: any) => {
     if (isNaN(threshold) || threshold >= 1 || threshold <= 0) {
-        throw Error("Must be a decimal greater then 0 but less than 1.");
+        throw Error("Must be a value greater than 0 but less than 1. For example, if you want the threshold to be 5%, then pass in `--noticeableThreshold=0.05`");
     }
 
     return threshold;
@@ -12,7 +12,7 @@ const threashhold = (threshold: any) => {
 
 program
     .option('-d, --dry-run', 'Run the performance tests without saving the results to `benchmark/results.json`', false)
-    .option('-t, --threshold <number>', 'integer argument', threashhold, defaultThreshold);
+    .option('-t, --threshold <number>', 'Must be a value greater than 0 but less than 1. For example, if you want the threshold to be 5%, then pass in `--noticeableThreshold=0.05`', threashhold, defaultThreshold);
 
 program.parse();
 program.opts()
