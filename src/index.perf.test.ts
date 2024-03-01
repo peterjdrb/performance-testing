cc50620c4f31b16d417ec4fcf3d06e2c9e3fdf3a
Peter Robertson <peterjdrb@users.noreply.github.com>
2024-03-01 23:06:56 +0000

    moved benchmark code to src

--- src/index.perf.test.ts
+++
@@ -1,7 +0,0 @@
-import { fibonacciRecursion } from "./index";
-
-export const performanceTest = () => {
-  for (let n = 1; n <= 40; n++) {
-    fibonacciRecursion(n);
-  }
-};
--- src/index4.perf.test.ts
+++
@@ -1,7 +0,0 @@
-import { asyncTest } from "./index";
-
-export const performanceTest = async () => {
-  for (let n = 1; n <= 500; n++) {
-    await asyncTest(n);
-  }
-};
